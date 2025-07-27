89a1065ed4efc26df76b792ba82f1739ae5d19c7
LeilaAyman <mariamaymanaliomar@gmail.com>
2025-07-27 23:34:35 +0300

    done

--- vendor-survey/src/firebase.js
+++ vendor-survey/src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef",
+  apiKey: "AIzaSyBz2Q5VUEjo1km59VmSlgjp5OfxXcx80O0",
+  authDomain: "vendor-evaluation-ee39e.firebaseapp.com",
+  projectId: "vendor-evaluation-ee39e",
+  storageBucket: "vendor-evaluation-ee39e.firebasestorage.app",
+  messagingSenderId: "864017817172",
+  appId: "1:864017817172:web:54cc6d3bcbb80eccefd7c1",
+  measurementId: "G-XNPYL40CL2"
 };
 
 export const app = initializeApp(firebaseConfig); 
